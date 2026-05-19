c373671d2594401628a6437b72b1f7c2460eae1a
Juan David Malambo Lopez <juanlopezyt123@gmail.com>
2026-05-19 17:37:29 -0500

    Mejoras en el backend: se agregó gunicorn a los requisitos para mejorar el rendimiento del servidor. Además, se realizaron ajustes en el código de main.py para optimizar la gestión de solicitudes y mejorar la eficiencia general del backend.

--- pagina/backend/main.py
+++ pagina/backend/main.py
@@ -1,8 +1,15 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for
 
+# Definimos las rutas relativas basadas en la ubicación de este archivo (main.py)
+# Como main.py está en 'pagina/backend', subimos un nivel con '../' para llegar a 'pagina'
+# y luego entramos a 'frontend'
+template_dir = os.path.join(os.path.dirname(__file__), '../frontend/templates')
+static_dir = os.path.join(os.path.dirname(__file__), '../frontend/static')
+
 app = Flask(__name__, 
-            template_folder='../Frontend/templates', 
-            static_folder='../Frontend/static')
+            template_folder=template_dir, 
+            static_folder=static_dir)
 
 # Ruta Principal (Landing Page)
 @app.route('/')
@@ -24,6 +31,7 @@ def curso2():
 def curso3():
     return render_template('curso3.html')
 
-
 if __name__ == '__main__':
+    # Nota: En Render, Gunicorn se encargará de ejecutar la app, 
+    # esto solo es para que funcione en tu PC.
     app.run(debug=True, port=5000)
